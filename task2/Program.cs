f7243b6c4dfefecea0f9606be6e8045b40473e5f
LarsonRagnar <larionov1992@gmail.com>
2023-03-07 17:39:47 +0300

    замена четных индексов в двумерном массиве

---
+++ task2/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 49: Задайте двумерный массив. 
+// Найдите элементы, у которых оба индекса чётные, 
+// и замените эти элементы на их квадраты.
+
+int[,] GetMatrix(int rows, int cols, int minValue, int maxValue)
+{
+    int[,] matrix = new int[rows, cols];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return matrix;
+}
+int[,] ChangeMatrix(int[,] inputMatrix)
+{
+    for (int i = 0; i < inputMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputMatrix.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && i % 2 == 0 && i != 0 && j != 0)
+            {
+                inputMatrix[i,j] *= inputMatrix[i,j];
+            }
+        }
+    }
+    return inputMatrix;
+}
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write(matr[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] resultMatrix = GetMatrix(3, 4, 0, 100);
+PrintMatrix(resultMatrix);
+Console.WriteLine("Матрица с заменеными индесами");
+PrintMatrix(ChangeMatrix(resultMatrix));
